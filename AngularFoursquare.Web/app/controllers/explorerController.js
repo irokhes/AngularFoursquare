0a48f2d2a74b45524b384c867968601f5aa9487c
irokhes <jorgereyero@gmail.com>
2015-02-01 21:09:24 +0000

    Add explorer explorer service for querying foursquare

--- AngularFoursquare.Web/app/controllers/explorerController.js
+++ AngularFoursquare.Web/app/controllers/explorerController.js
@@ -1,3 +1,49 @@
-﻿app.controller('ExplorerController', function($scope) {
+﻿'use strict';
+app.controller('ExplorerController',['explorerService'], function ($scope, $explorerService) {
     $scope.explorerNearBy = "London";
+    $scope.explorerQuery = "";
+    $scope.filterValue = "";
+
+
+    $scope.places = [];
+    $scope.filterPlaces = [];
+    $scope.filteredPlacesCount = 0;
+
+    $scope.totalRecordsCount = 0;
+    $scope.paseSize = 10;
+    $scope.currentPage = 1;
+
+    init();
+
+    function init() {
+        createWatche();
+        getPlaces();
+    }
+
+    function getPlaces() {
+        var offset = ($scope.paseSize) * ($scope.currentPage - 1);
+
+        $explorerService.get({ near: $scope.exploreNearby, query: $scope.exploreQuery, limit: $scope.pageSize, offset: offset }, function(placesResult) {
+            if (placesResult.response.groups) {
+                $scope.places = placesResult.response.groups[0].items;
+                $scope.totalRecordsCount = placesResult.response.totalResults;
+                filterPlaces('');
+            } else {
+                $scope.filterPlaces = [];
+                $scope.totalRecordsCount = 0;
+            }
+        });
+
+    };
+
+    function filterPlaces(filterInput) {
+        $scope.filterPlaces = $filter("placeNameCategoryFilter")($scope.places, filterInput);
+        $scope.filteredPlacesCount = $scope.filterPlaces.length;
+    }
+
+    function createWatche() {
+        $scope.$watch("filterValue", function(filterInput) {
+            filterPlaces(filterInput);
+        });
+    }
 });
---
+++ AngularFoursquare.Web/app/services/explorerService.js
@@ -0,0 +1,22 @@
+﻿var requestParams = {
+    clientId: "TGFG15JODGFPEDPOIX42URX1YYLTVZMXMXX1UY4BPM3YVF10",
+    clientSecret: "O5E1J3ZFJM4XH12Z0VLEFMT3MRNKHQQSDAGCT5GIKOWE12UQ",
+    version: "20130815"
+}
+
+app.provider('explorerService', function($resource) {
+    var requestUri = 'https://api.foursquare.com/v2/venues/:action';
+    
+    return $resource(requestUri,
+    {
+        action: 'explore',
+        client_id: requestParams.clientId,
+        client_secret: requestParams.clientSecret,
+        v: requestParams.version,
+        venuePhotos: '1',
+        callback: 'JSON_CALLBACK'
+    },
+    {
+        get: { method: 'JSONP' }
+    });
+});
